dd4c8563077324a84dc5516636f2c8f07d487921
EncryptedCoder <kyle2.l.bush@gmail.com>
2025-05-29 14:57:35 -0800

    Update build_encodeme.sh

--- apple-positional-audio-codec-invalid-header-main/build_encodeme.sh
+++ apple-positional-audio-codec-invalid-header-main/build_encodeme.sh
@@ -1,1 +1,4 @@
-clang++ -g -Os -o encodeme -std=c++23 -fmodules -fcxx-modules -fobjc-arc encodeme.mm
+#!/bin/bash
+set -e
+clang++ -g -Os -std=c++2b -fmodules -fcxx-modules -fobjc-arc -arch x86_64 -arch arm64 -isysroot $(xcrun --sdk iphonesimulator --show-sdk-path) -framework AVFAudio -framework AudioToolbox encodeme.mm -o encodeme
+chmod +x encodeme
